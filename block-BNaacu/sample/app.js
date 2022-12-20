fb6df22b35c40df3bca257569b34c9c35362361b
anup kumar <kanup414@gmail.com>
2022-12-21 01:28:56 +0530

    done

--- block-BNaacu/sample/app.js
+++ block-BNaacu/sample/app.js
@@ -1,18 +1,23 @@
 var express = require('express');
-
+var studentsRouter = require('./routes/students')
 var app = express();
 
 
 //setup view engine
 app.set("view engine", "ejs");
-app.set('views', __dirname + '/school/views')
+app.set('views', __dirname + '/views')
+
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
-})
+});
 
+app.use('/students', studentsRouter);
+
+//handle error
 app.use((req, res, next) => {
-    res.send('Page Not Found');
+    res.status(404).send('Page Not Found');
 })
 
 app.listen(3000, () => {
