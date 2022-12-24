b9fe48d10e8c55a99e9e4c907d50e27de2e9d8aa
anup kumar <kanup414@gmail.com>
2022-12-24 11:34:06 +0530

    done

--- block-BNaacy/sample/app.js
+++ block-BNaacy/sample/app.js
@@ -1,14 +1,37 @@
 var express = require('express');
+var path = require('path');
+var userRouter = require('./routes/users')
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/sample",
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+},
+(err) => {
+    console.log(err ? "connected false" : "connected true");
+});
+
 var app = express();
 
-app.use('/', (req, res) => {
-    res.send('welcome');
-})
+//midlleware
+app.set('view engine', "ejs");
+app.set('views', path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: false }));
+
+//routes
+// app.use('/', (req, res) => {
+//     res.render('index.ejs');
+// });
+
+app.use('/users', userRouter);
+
+//error handler
 app.use((req, res, next) => {
-    res.send(err);
-})
+    res.status(404).send('Page Not Found');
+});
 
-app.listen(3000, (req, res) => {
+app.listen(3000, () => {
     console.log('server is listening on port 3k');
-})
+});
